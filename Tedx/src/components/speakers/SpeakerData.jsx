8142e5b739358717ee885bf2f5838f8d8348e7ca
Venkatesh Badarala <21PA1A1206@vishnu.edu.in>
2025-02-16 19:56:09 +0530

    speakers

--- Tedx/src/components/speakers/SpeakerData.jsx
+++ Tedx/src/components/speakers/SpeakerData.jsx
@@ -6,6 +6,7 @@ import MokshaKalyanram from '../../assets/images/speakerImage/MokshaKalyanram.pn
 import BijayBairagi from '../../assets/images/speakerImage/BijayBairagi.png';
 import KowshikMaridi from '../../assets/images/speakerImage/KowshikMaridi.png';
 import GallaMadhavi from '../../assets/images/speakerImage/GallaMadhavi.png';
+import Bhaskar from '../../assets/images/speakerImage/Bhaskar.png';
 
 
 export const speakers = [
@@ -13,46 +14,37 @@ export const speakers = [
     name: 'Mr. Ravi Mantri',
     role: 'Author & Content Creator',
     image: RaviMantri,
-    LinkedInLink: '',
-    InstagramLink: '',
-    MailLink: '',
     description: 'Ravi Mantri was born in Andhra Pradesh, South India. He pursued his masters degree from the College of Engineering, JNTU, before moving to Ireland for work. As a techie by profession, Ravi embarked on his literary journey in 2019 with his debut novel, Amma Dairylo Konni Pageelu. The novel became a best-seller and won the hearts of many readers. In addition to novels, Ravi has penned several short stories that have been published in renowned Telugu magazines. An avid social media influencer, he actively creates relatable content for his followers. Ravi is currently working on his second novel.'
   },
   {
     name: 'Dr. Moksha',
     role: 'Advocate',
     image: MokshaKalyanram,
-    LinkedInLink: '',
-    InstagramLink: '',
-    MailLink: '',
     description: 'Dr. Moksha Kalyanram Abhiramula, a seasoned legal expert, with over more than two decades of proficiency in corporate, taxation, civil, and ADR laws. Leading La Mintage Legal LLP, law firm specialized in providing solutions to complex corporate, IBC, Tax and property matters. Recognized as the "Entrepreneur of the Year 2022" and a "Game Changer," his vision extends to revolutionizing global dispute resolution. As the driving force behind La Mintage Dispute Resolution Hub LLP, he leverages modern technology to offer transparent mediation, conciliation, and arbitration services. His exemplary journey reflects a passion for preventing corporate fraud and fostering mutually amicable dialogues, earning him accolades and trust.'
   },
  {
     name: 'Mr. Kowshik Maridi',
     role: 'Finacial Expert',
     image: KowshikMaridi,
-    LinkedInLink: '',
-    InstagramLink: '',
-    MailLink: '',
     description: 'Kowshik Maridi was born in Bhimavaram, Andhra Pradesh. He pursued an MBA in Finance after discontinuing his CA studies and later ventured into financial education. In 2019, he started his YouTube channel to spread financial awareness among Telugu-speaking audiences. Today, his channel is the most-subscribed personal finance channel in South India, with over 5 million subscribers.Beyond YouTube, Kowshik has built a strong presence on social media, becoming the first personal finance creator in South India to achieve 1 million followers on Instagram, now surpassing 1.3 million. He also has a growing community of 550,000 followers on Facebook. His engaging content on personal finance, investments, and cybercrime awareness has made him one of the most influential finance creators in the region. Kowshik has also collaborated with law enforcement and industry experts to educate people on financial security.'
   },
   {
     name: 'Mrs. Galla Madhavi',
     role: 'Director at Vikaas Hospitals & MLA, Guntur West',
     image: GallaMadhavi,
-    LinkedInLink: '',
-    InstagramLink: '',
-    MailLink: '',
     description: 'Galla Madhavi, a dedicated social worker from Mulukuduru village, is committed to public service and community development. With an MCA degree, she initially pursued Civil Services but later found her calling in teaching and philanthropy. She currently serves as the Director of Vikaas Hospitals, striving to provide affordable and quality healthcare. As the co-founder of the Sri Bhramara Trust, she leads initiatives in education, healthcare, and social welfare, focusing on youth empowerment and environmental sustainability. Actively engaged in community service, she organizes free health camps, distributes essential supplies to the needy, and supports employment programs. Through her political campaign, she actively engages with people at the grassroots level, addressing their concerns and working towards their well-being. With her unwavering dedication, she envisions a society where every individual has access to better opportunities and an improved quality of life.'
   },
   {
     name: 'Mr. Bijay Bairagi',
     role: 'Supply Chain Management  Head at  East West Seeds',
     image: BijayBairagi,
-    LinkedInLink: '',
-    InstagramLink: '',
-    MailLink: '',
     description: 'Postgraduate in Agriculture and in General Management; an alumna of IIM Calcutta and ISB Hyderabad, Bijay Bairagi brings in rich academic knowledge. Currently associated with East West Seed International as Head of Supply Chain Management, he brings in three decades of extensive experience in the Sales and Supply Chain functions working with the prestigious MNCs and Indian organizations. A practitioner of yoga and meditation, Bijay holds deep understanding of spiritual practices and wellbeing. He loves to play “Tabla”, listens to Indian Classical music and reads voraciously in his free time.'
+  },
+  {
+    name: 'Dr. JVS Bhaskar',
+    role: 'Founder & CEO, Inspire International Foundation',
+    image: Bhaskar,
+    description: 'Dr. JVS Bhaskar, the visionary Founder and CEO of INSPIRE International Foundation, has established himself as a beacon of hope and inspiration for youth across India. Hailing from a humble village in Andhra Pradesh, his rise to prominence as a motivational speaker and soft skills trainer is a testament to his unshakable commitment to service, leadership, and upliftment of society. Through INSPIRE - Ray of Life, he has impacted thousands of lives, transforming despair into determination and guiding countless youth towards the path of success.Born to Juttada Manikyam and Appalaraju in the village of Indugapalli, East Godavari District, Dr. Bhaskars journey was anything but easy. From a young age, he exhibited exceptional leadership qualities and a strong inclination toward public service. His dedication to education and service was evident as he grew into a role model for his peers. Even during his school days, he earned respect and recognition for his motivational speeches, often being invited to address gatherings of students and educators alike. This early exposure to public speaking and his deep passion for inspiring others set the foundation for his future endeavors.'
   },
   
 ];
